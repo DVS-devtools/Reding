d14a831727f8a0be7f3b941849835101bcf32c86
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2013-02-01 17:39:48 +0100

    Adding first setup.py, need more work.

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import sys
+import os
+
+wd = os.path.dirname(os.path.abspath(__file__))
+os.chdir(wd)
+sys.path.insert(1, wd)
+
+name = 'Reding'
+pkg = __import__('reding')
+
+author, email = pkg.__author__.rsplit(' ', 1)
+email = email.strip('<>')
+
+version = pkg.__version__
+classifiers = pkg.__classifiers__
+
+readme = open(os.path.join(wd, 'README.md'),'r').readlines()
+description = readme[3]
+long_description = ''.join(readme)
+
+try:
+    reqs = open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).read()
+except (IOError, OSError):
+    reqs = ''
+
+setup(
+    name=name,
+    version=version,
+    author=author,
+    author_email=email,
+    url='https://github.com/BuongiornoMIP/Reding',
+    maintainer=author,
+    maintainer_email=email,
+    description=description,
+    long_description=long_description,
+    classifiers=classifiers,
+    install_requires = reqs,
+    packages=find_packages(),
+    license = 'The MIT License (MIT)',
+    keywords ='Rating REST Redis Flask',
+)
