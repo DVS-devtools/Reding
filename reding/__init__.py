f407e10e4caeef38aa7d84763f1c5e047ea4f26e
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2013-02-05 15:49:59 +0100

    Version number changed to 0.1.3

--- reding/__init__.py
+++ reding/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Giorgio Salluzzo <giorgio.salluzzo@gmail.com>'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __classifiers__ = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
